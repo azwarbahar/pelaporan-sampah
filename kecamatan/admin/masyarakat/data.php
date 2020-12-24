304972c6be79b2561292c1f1ce689081621ccea5
azwarbahar07 <azwarbahar07@gmail.com>
2020-12-24 10:04:11 +0800

    add data masyarakat

---
+++ kecamatan/admin/masyarakat/data.php
@@ -0,0 +1,126 @@
+<?php
+require_once '../template/header/header.php';
+$masyarakat = mysqli_query($conn, "SELECT * FROM tb_masyarakat");
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Data Masyarakat</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/pelaporan-sampah/kelurahan/admin/">Home</a></li>
+              <li class="breadcrumb-item active">Data Masyarakat</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+
+            <div class="card">
+              <div class="card-header">
+                <label for="inputName">Kelurahan</label>
+                <div class="row">
+                  <div class="col-4">
+                    <select class="form-control select2" style="width: 100%;" name="kelurahan_masyarakat" id="kelurahan_masyarakat">
+                      <option selected="selected" value="-">- Semua -</option>
+                      <option value="Balang Baru">Balang Baru</option>
+                      <option value="Barombong">Barombong</option>
+                      <option value="Bongaya">Bongaya</option>
+                      <option value="Bonto Duri">Bonto Duri</option>
+                      <option value="Jongaya">Jongaya</option>
+                      <option value="Maccini Sombala">Maccini Sombala</option>
+                      <option value="Mangasa">Mangasa</option>
+                      <option value="Mannuruki">Mannuruki</option>
+                      <option value="Pa'baeng-Baeng">Pa'baeng-Baeng</option>
+                      <option value="Parang Tambung">Parang Tambung</option>
+                      <option value="Tanjung Merdeka">Tanjung Merdeka</option>
+                    </select>
+                  </div>
+
+                  <div class="col-8">
+                  <a href="invoice-print.html" target="_blank" class="btn btn-info float-right"><i class="fas fa-print"></i> Print</a>
+                  </div>
+
+                </div>
+                <!-- <a href="tambah.php" type="button" class="btn btn-primary"><i class="fa fa-plus-square"></i>&nbsp Tambah Masyarakat</a> -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>NIK</th>
+                    <th>Nama</th>
+                    <th>Kelurahan</th>
+                    <th>Telpon</th>
+                    <th>Iuran</th>
+                    <th>Status</th>
+                    <th>Aksi</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <?php $i = 1; foreach($masyarakat as $dta) { ?>
+                  <tr>
+                    <td style="text-align:center"><?= $i ?></td>
+                    <td><?= $dta['nik_masyarakat'] ?></td>
+                    <td><?= $dta['nama_masyarakat'] ?></td>
+                    <td><?= $dta['kelurahan_masyarakat'] ?></td>
+                    <td><?= $dta['telpon_masyarakat'] ?></td>
+                    <?php
+                      if ($dta['pembayaran_masyarakat'] == "Sudah"){
+                        echo "<td style='text-align:center'><span class='badge bg-primary'>Sudah</span></td>";
+                      } else if ($dta['pembayaran_masyarakat'] == "Belum"){
+                        echo "<td style='text-align:center'><span class='badge bg-warning'>Belum</span></td>";
+                      }
+                    ?>
+                    <?php
+                      if ($dta['status_masyarakat'] == "Aktif"){
+                        echo "<td style='text-align:center'><span class='badge bg-success'>Aktif</span></td>";
+                      } else if ($dta['status_masyarakat'] == "Non Aktif"){
+                        echo "<td style='text-align:center'><span class='badge bg-danger'>Non Aktif</span></td>";
+                      }
+                    ?>
+                    <td style="text-align:center">
+                        <a href="detail.php?id_masyarakat=<?= $dta['id_masyarakat'] ?>" type="button" class="btn btn-secondary"><i class="fa fa-eye"></i></a>
+                        <!-- <a href="edit.php?id_masyarakat=<?= $dta['id_masyarakat'] ?>" type="button" class="btn btn-warning"><i class="fa fa-edit"></i></a> -->
+                        <!-- <a href="#" type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-danger<?= $dta['id_masyarakat'] ?>" ><i class="fa fa-trash"></i></a> -->
+                    </td>
+                  </tr>
+
+                  <?php $i = $i + 1; } ?>
+                  </tbody>
+
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+<?php
+require '../template/footer/footer.php';
+?>
---
+++ kecamatan/admin/masyarakat/detail.php
@@ -0,0 +1,398 @@
+<?php
+require '../template/header/header.php';
+
+$id_masyarakat = $_GET['id_masyarakat'];
+$result = mysqli_query($conn, "SELECT * FROM tb_masyarakat WHERE id_masyarakat = '$id_masyarakat'");
+$dta = mysqli_fetch_assoc($result);
+
+?>
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <a href="data.php" class="btn btn-secondary"><i class="fa fa-arrow-left"></i> Back</a>
+          <h1 class="m-0 text-dark">Data Masyarakat</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="/pelaporan-sampah/kelurahan/admin/">Home</a></li>
+            <li class="breadcrumb-item"><a href="/pelaporan-sampah/kelurahan/admin/layout/profile-dpr/data.php">Data Masyarakat</a></li>
+            <li class="breadcrumb-item active">Detail</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+
+  <!-- Main content -->
+  <section class="content">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Profil</h3>
+
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                  <i class="fas fa-minus"></i></button>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-4">
+                  <!-- Profile Image -->
+                  <div class="card card-primary card-outline">
+                    <div class="card-body box-profile">
+                      <div class="text-center">
+                        <img class="img-fluid" src="/pelaporan-sampah/kelurahan/admin/masyarakat/foto/<?= $dta['foto_masyarakat'] ?>" style="max-width: 280px; max-height: 350px; min-width: 280px; min-height: 350px;" alt="User profile picture">
+                      </div>
+                      <br>
+                      <h4 class="text-center"><?= $dta['nama_masyarakat'] ?></h4>
+                      <p class="text-muted text-center"><?= $dta['nik_masyarakat'] ?></p>
+                      <ul class="list-group list-group-unbordered mb-3"></ul>
+
+                      <a href="edit.php?id_masyarakat=<?= $dta['id_masyarakat'] ?>" class="btn btn-danger btn-block"><b>Edit Profile</b></a>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+
+                  <!-- About Me Box -->
+                  <div class="card card-secondary">
+                    <div class="card-header">
+                      <h3 class="card-title">Data Masyarakat</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+
+                      <strong><i class="fas fa-map-marker-alt mr-1"></i> Alamat</strong>
+                      <p class="text-muted">
+                        <?= $dta['alamat_masyarakat'] ?>
+                      </p>
+                      <hr>
+
+                      <strong><i class="fas fa-map-marker-alt mr-1"></i> Kelurahan</strong>
+                      <p class="text-muted">
+                        <?= $dta['kelurahan_masyarakat'] ?>
+                      </p>
+                      <?php
+                      $result1 = mysqli_query($conn, "SELECT * FROM tb_area WHERE id_area = '$dta[area_masyarakat]'");
+                      $dta1 = mysqli_fetch_assoc($result1);
+                      ?>
+                      <p class="text-muted"><b>Area :</b> <?= $dta1['nama_area'] ?></p>
+                      <hr>
+
+                      <strong><i class="fas fa-phone mr-1"></i> Telpon</strong>
+                      <p class="text-muted"><?= $dta['telpon_masyarakat'] ?></p>
+                      <hr>
+
+                      <strong><i class="fas fa-circle mr-1"></i> Usia</strong>
+                      <p class="text-muted"><?= $dta['telpon_masyarakat'] ?></p>
+                      <hr>
+
+                      <strong><i class="fas fa-circle mr-1"></i>Status Akun</strong>
+                      <p class="text-muted">
+                      <?php
+                        if ($dta['status_masyarakat'] == "Aktif"){
+                          echo "<span class='badge bg-success'>Aktif</span>";
+                        } else if ($dta['status_masyarakat'] == "Non Aktif"){
+                          echo "<span class='badge bg-danger'>Non Aktif</span>";
+                        }
+                      ?>
+                      </p>
+                      <hr>
+
+                      <strong><i class="fas fa-circle mr-1"></i> Pembayaran</strong>
+                      <p class="text-muted">
+                      <?php
+                        if ($dta['pembayaran_masyarakat'] == "Sudah"){
+                          echo "<span class='badge bg-success'>Sudah</span>";
+                        } else if ($dta['pembayaran_masyarakat'] == "Belum"){
+                          echo "<span class='badge bg-danger'>Belum</span>";
+                        }
+                      ?>
+                      </p>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+
+                <div class="col-md-8">
+                  <div class="card card-success">
+                    <div class="card-header">
+                      <h3 class="card-title">Data Laporan</h3>
+                      <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                        <i class="fas fa-minus"></i></button>
+                      </div>
+                    </div><!-- /.card-header -->
+                    <div class="card-body">
+
+                      <div class="row">
+                        <div class="col-12">
+                          <div class="card">
+                            <div class="card-header">
+                              <h3 class="card-title">List Laporan</h3>
+
+                              <div class="card-tools">
+                                <div class="input-group input-group-sm" style="width: 150px;">
+                                  <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                                  <div class="input-group-append">
+                                    <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0" style="max-height: 500px; height: fit-content;">
+                              <table class="table table-head-fixed text-nowrap">
+                                <thead>
+                                  <tr>
+                                    <th>ID</th>
+                                    <th>User</th>
+                                    <th>Date</th>
+                                    <th>Status</th>
+                                    <th>Reason</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                            <!-- /.card-body -->
+                          </div>
+                          <!-- /.card -->
+                        </div>
+                      </div>
+                      <!-- /.row -->
+                    </div>
+                    <!-- /.nav-tabs-custom -->
+                  </div>
+                  <!-- /.col -->
+
+                  <div class="card card-warning">
+                    <div class="card-header">
+                      <h3 class="card-title">Data Keluhan</h3>
+                      <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                        <i class="fas fa-minus"></i></button>
+                      </div>
+                    </div><!-- /.card-header -->
+                    <div class="card-body">
+
+                      <div class="row">
+                        <div class="col-12">
+                          <div class="card">
+                            <div class="card-header">
+                              <h3 class="card-title">List Keluhan</h3>
+
+                              <div class="card-tools">
+                                <div class="input-group input-group-sm" style="width: 150px;">
+                                  <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                                  <div class="input-group-append">
+                                    <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0" style="height: 500px;">
+                              <table class="table table-head-fixed text-nowrap">
+                                <thead>
+                                  <tr>
+                                    <th>ID</th>
+                                    <th>User</th>
+                                    <th>Date</th>
+                                    <th>Status</th>
+                                    <th>Reason</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                  <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                            <!-- /.card-body -->
+                          </div>
+                          <!-- /.card -->
+                        </div>
+                      </div>
+                      <!-- /.row -->
+                    </div>
+                    <!-- /.nav-tabs-custom -->
+                  </div>
+                  <!-- /.col -->
+                </div>
+
+              </div>
+
+
+            </div>
+
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.content -->
+
+
+</div>
+<!-- /.content-wrapper -->
+
+
+<?php
+require '../template/footer/footer.php';
+?>
