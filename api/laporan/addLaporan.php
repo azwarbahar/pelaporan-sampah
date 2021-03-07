f7196ab5cd7db3b3db018e3ac147989b8848c391
azwarbahar07 <azwarbahar07@gmail.com>
2021-03-07 19:22:54 +0800

    api tambah laporan

---
+++ api/laporan/addLaporan.php
@@ -0,0 +1,80 @@
+<?php
+ require_once '../../koneksi.php';
+
+    $keterangan_laporan = $_POST['keterangan_laporan'];
+    $foto_laporan = $_POST['foto_laporan'];
+    $latitude_laporan = $_POST['latitude_laporan'];
+    $longitude_laporan = $_POST['longitude_laporan'];
+    $alamat_laporan = $_POST['alamat_laporan'];
+    $nik_laporan = $_POST['nik_laporan'];
+    $kelurahan_laporan = $_POST['kelurahan_laporan'];
+    $area_laporan = $_POST['area_laporan'];
+    $petugas_id = "";
+    $masyarakat_id = $_POST['masyarakat_id'];
+    $staus_laporan = "Proccess";
+
+    $query = "INSERT INTO tb_laporan values(null,'$keterangan_laporan',
+                                            '$foto_laporan',
+                                            '$latitude_laporan',
+                                            '$longitude_laporan',
+                                            '$alamat_laporan',
+                                            '$nik_laporan',
+                                            '$kelurahan_laporan',
+                                            '$area_laporan',
+                                            '$petugas_id',
+                                            '$masyarakat_id',
+                                            '$staus_laporan',
+                                            null,
+                                            null)";
+
+        if ( mysqli_query($conn, $query) ){
+
+            if ($foto_laporan == null) {
+
+                $finalPath = "../../assets/dist/img/laporan/img_default.png";
+                $result["kode"] = "1";
+                $result["pesan"] = "Success";
+
+                echo json_encode($result);
+                mysqli_close($conn);
+
+            } else {
+
+                $id = mysqli_insert_id($conn);
+                $path = "../../assets/dist/img/laporan/image_".time().".png";
+                $finalPath = "image_".time().".png";
+
+                $insert_picture = "UPDATE tb_laporan SET foto_laporan='$finalPath' WHERE id_laporan='$id' ";
+
+                if (mysqli_query($conn, $insert_picture)) {
+
+                    if ( file_put_contents( $path, base64_decode($foto_laporan) ) ) {
+
+                        $result["kode"] = "1";
+                        $result["pesan"] = "Success!";
+
+                        echo json_encode($result);
+                        mysqli_close($conn);
+
+                    } else {
+
+                        $response["kode"] = "0";
+                        $response["pesan"] = "Error! ".mysqli_error($conn);
+                        echo json_encode($response);
+
+                        mysqli_close($conn);
+                    }
+
+                }
+            }
+
+        }
+        else {
+            $response["kode"] = "0";
+            $response["pesan"] = "Error! ".mysqli_error($conn);
+            echo json_encode($response);
+
+            mysqli_close($conn);
+        }
+
+?>
