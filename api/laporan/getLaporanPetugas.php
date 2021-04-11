90d9480066ad5167aedd0f0eefd72f5da5e01000
azwarbahar07 <azwarbahar07@gmail.com>
2021-04-11 19:54:33 +0800

    add restfill riwayat

---
+++ api/laporan/getLaporanPetugas.php
@@ -0,0 +1,18 @@
+<?php
+ require_once '../../koneksi.php';
+ header('Content-type: application/json');
+
+$petugas_id = $_GET["petugas_id"];
+$query = "SELECT * FROM tb_laporan WHERE petugas_id = '$petugas_id' AND ( staus_laporan = 'Done' OR staus_laporan = 'Cancel' ) ORDER BY id_laporan DESC";
+
+ $result = mysqli_query($conn, $query);
+
+ $array = array();
+ while($row = mysqli_fetch_assoc($result)){
+     $array[] = $row;
+ }
+
+ echo ($result) ?
+ json_encode(array("kode" => "1", "result_laporan_petugas" => $array)) :
+ json_encode(array("kode" => "0", "pesan" => "Data tidak ditemukan"));
+?>
