ab6c39fa7787c132675f01e30162df5065d99493
azwarbahar07 <azwarbahar07@gmail.com>
2021-04-14 23:43:28 +0800

    change detail masyarakat

--- kelurahan/admin/masyarakat/detail.php
+++ kelurahan/admin/masyarakat/detail.php
@@ -159,84 +159,27 @@ $dta = mysqli_fetch_assoc($result);
                               <table class="table table-head-fixed text-nowrap">
                                 <thead>
                                   <tr>
-                                    <th>ID</th>
-                                    <th>User</th>
-                                    <th>Date</th>
+                                    <th>No</th>
+                                    <th>Keterangan</th>
                                     <th>Status</th>
-                                    <th>Reason</th>
+                                    <th>Waktu</th>
+                                    <th></th>
                                   </tr>
                                 </thead>
                                 <tbody>
+                                  <?php
+                                   $i = 1;
+                                   $laporan = mysqli_query($conn, "SELECT * FROM tb_laporan WHERE masyarakat_id = '$dta[id_masyarakat]'");
+                                   foreach($laporan as $dta_laporan) {
+                                  ?>
                                   <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
+                                    <td style="text-align:center"><?= $i ?></td>
+                                    <td><?= $dta_laporan['keterangan_laporan'] ?></td>
+                                    <td><span class="tag tag-success"><?= $dta_laporan['staus_laporan'] ?></span></td>
+                                    <td><?= $dta_laporan['created_at'] ?></td>
+                                    <td>Bacon</td>
                                   </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
+                                <?php $i = $i + 1; } ?>
                                 </tbody>
                               </table>
                             </div>
@@ -261,114 +204,6 @@ $dta = mysqli_fetch_assoc($result);
                     </div><!-- /.card-header -->
                     <div class="card-body">
 
-                      <div class="row">
-                        <div class="col-12">
-                          <div class="card">
-                            <div class="card-header">
-                              <h3 class="card-title">List Keluhan</h3>
-
-                              <div class="card-tools">
-                                <div class="input-group input-group-sm" style="width: 150px;">
-                                  <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                                  <div class="input-group-append">
-                                    <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body table-responsive p-0" style="height: 500px;">
-                              <table class="table table-head-fixed text-nowrap">
-                                <thead>
-                                  <tr>
-                                    <th>ID</th>
-                                    <th>User</th>
-                                    <th>Date</th>
-                                    <th>Status</th>
-                                    <th>Reason</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                  <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>Bacon ipsum dolor </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </div>
-                            <!-- /.card-body -->
-                          </div>
-                          <!-- /.card -->
-                        </div>
-                      </div>
-                      <!-- /.row -->
                     </div>
                     <!-- /.nav-tabs-custom -->
                   </div>
