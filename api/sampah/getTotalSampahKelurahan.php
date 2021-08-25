7e9d134340cfa174201f7069ef6b471e907b249b
azwarbahar07 <azwarbahar07@gmail.com>
2021-08-26 04:34:05 +0800

    p

--- api/sampah/getTotalSampahKelurahan.php
+++ api/sampah/getTotalSampahKelurahan.php
@@ -5,12 +5,12 @@
 $kelurahan = $_GET["kelurahan"];
  // total Berat
  $result= mysqli_query($conn,"SELECT SUM(berat_sampah) AS total_berat FROM tb_laporan_petugas WHERE kelurahan = '$kelurahan' ");
- $row = mysqli_fetch_assoc($result)
+ $row = mysqli_fetch_assoc($result);
 
  if ($result) { 
      echo json_encode(array(
                                 "kode" => "1",
-                                "total_berat_kelurahan" => $$row['total_berat'],
+                                "total_berat_kelurahan" => $row['total_berat'],
      ));       
  } else {
     echo json_encode(array("kode" => "0", "pesan" => "Data tidak ditemukan"));
