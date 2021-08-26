1b66ae09bf1ca72b9f821c2713c3f4a6537c82df
azwarbahar07 <azwarbahar07@gmail.com>
2021-08-26 19:47:59 +0800

    lupa

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set("Asia/Makassar");
 session_start();
-$conn = mysqli_connect("localhost", "mtrtamalate_user", "mtrtamalate_db", "mtrtamalate_db");
+$conn = mysqli_connect("localhost", "u3737783_mtrtamalate_user", "mtrtamalate_db", "u3737783_mtrtamalate_db");
 
 ?>
