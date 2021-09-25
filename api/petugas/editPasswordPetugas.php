635290557048daa347aae755a00b906dcf606425
azwarbahar07 <azwarbahar07@gmail.com>
2021-09-25 15:10:36 +0800

    dfdrghkj

--- api/petugas/editPasswordPetugas.php
+++ api/petugas/editPasswordPetugas.php
@@ -13,11 +13,11 @@ if (password_verify($password_lama, $data["password"])) {
 	$password = password_hash($password_baru, PASSWORD_DEFAULT);
     $query =  mysqli_query($conn, "UPDATE tb_pekerja SET password = '$password' WHERE id_pekerja  = '$id_petugas'");
     if ($query){
-        echo json_encode(array("kode" => 1, "pesan" => "Berhasil Mengubah Password"));
+        echo json_encode(array("kode" => "1", "pesan" => "Berhasil Mengubah Password"));
     } else {
-        echo json_encode(array("kode" => 2, "pesan" => "Terjadi Kesalahan Saat Mengubah Password"));
+        echo json_encode(array("kode" => "2", "pesan" => "Terjadi Kesalahan Saat Mengubah Password"));
     }
 } else{
-    echo json_encode(array("kode" => 0, "pesan" => "Password lama tidak sesuai"));
+    echo json_encode(array("kode" => "0", "pesan" => "Password lama tidak sesuai"));
 }
 ?>
